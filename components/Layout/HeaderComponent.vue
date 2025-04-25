7c519d19885e0ce2481d6f6ad109a8d0290c417c
luke-gibson <hello@lukegibsondesign.co.uk>
2025-04-25 20:56:15 +0100

    feat: wrap logo in NuxtLink for improved navigation

--- components/Layout/HeaderComponent.vue
+++ components/Layout/HeaderComponent.vue
@@ -20,7 +20,9 @@ watch(isMobileNavOpen, (val) => {
 <template>
   <header class="fixed top-0 left-0 right-0 max-w-[1440px] mx-auto">  
     <div class="bg-white bg-opacity-20 backdrop-blur-md flex justify-between items-center py-3 px-4 rounded-xl mx-4 mt-5 md:mt-8 z-20 relative">
-      <IconsLogo class="w-32 md:w-42"/>
+      <NuxtLink to="/" aria-label="oval creative logo and link to home page">
+        <IconsLogo class="w-32 md:w-42"/>
+      </NuxtLink>
       <LayoutNavigationComponent class="hidden md:block" />
       <ButtonsSecondaryButton class="hidden md:block" to="/">get started <IconsSpeech class="w-5 inline ml-2"/></ButtonsSecondaryButton>
       <LayoutHamburgerComponent :isActive="isMobileNavOpen" />

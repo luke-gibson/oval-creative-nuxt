6c926d0c5face4e1866f1db4a5517f1de5fa2883
Luke Gibson <luke@lukegibson.co.uk>
2026-01-30 12:49:28 +0000

    updated key to be public again not in env file lol

--- pages/packages/custom.vue
+++ pages/packages/custom.vue
@@ -31,8 +31,7 @@ const submitError = ref(false)
 const hcaptchaToken = ref('')
 const hcaptchaError = ref(false)
 
-const config = useRuntimeConfig()
-const hcaptchaSiteKey = config.public.captchaKey
+const hcaptchaSiteKey = '6cf2670e-e2ce-4a62-a33c-201cd7384d1a'
 
 const featureOptions = [
   'E-commerce functionality',

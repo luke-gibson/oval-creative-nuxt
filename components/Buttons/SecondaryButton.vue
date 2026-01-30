c798107f60c19f1112f6b95d1e5e3c5d338350b9
Luke Gibson <luke@lukegibson.co.uk>
2026-01-30 21:56:37 +0000

    rounded secondary button

--- components/Buttons/SecondaryButton.vue
+++ components/Buttons/SecondaryButton.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink 
-    class="text-md bg-secondary hover:bg-white text-primary hover:text-primary transition-all rounded-full px-6 pt-[7px] pb-2 inline-block font-medium">
+    class="text-md bg-secondary hover:bg-white text-primary hover:text-primary transition-all rounded-xl px-6 pt-[7px] pb-2 inline-block font-medium">
     <slot/>
   </NuxtLink>
 </template>
